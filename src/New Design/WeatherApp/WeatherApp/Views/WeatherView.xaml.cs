bc3ac0e592445c19b216c37adeb4e72a53cdcb4e
David Ortinau <david.ortinau@microsoft.com>
2017-10-23 19:49:09 -0500

    menu attempt, currently blanks out the app

--- src/New Design/WeatherApp/WeatherApp/Views/WeatherView.xaml.cs
+++ src/New Design/WeatherApp/WeatherApp/Views/WeatherView.xaml.cs
@@ -7,11 +7,13 @@ namespace WeatherApp.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class WeatherView : ContentPage
 	{
+		WeatherViewModel _vm;
+
 		public WeatherView()
 		{
 			InitializeComponent();
 
-			BindingContext = new WeatherViewModel
+			BindingContext = _vm = new WeatherViewModel
 			{
 				Navigation = Navigation
 			};
@@ -24,7 +26,49 @@ namespace WeatherApp.Views
 				await ((WeatherViewModel)BindingContext).GetWeatherAsync();
 			}
 
+			InitMenu();
+
 			base.OnAppearing();
+		}
+
+		void InitMenu()
+		{
+			var mainMenu = new Menu();
+
+			var fileMenu = new Menu { Text = "File" };
+			var exitItem = new MenuItem
+			{
+				Text = "Exit",
+				Command = new Command((obj) =>
+				{
+					Application.Current.Quit();
+				})
+			};
+			MenuItem.SetAccelerator(exitItem, Accelerator.FromString("cmd+q"));
+			fileMenu.Items.Add(exitItem);
+
+			var locationMenu = new Menu { Text = "Location" };
+			var changeItem = new MenuItem
+			{
+				Text = "Change",
+				Command = new Command((obj) =>
+				{
+					Navigation.PushModalAsync(new LocationEntryPage());
+				})
+			};
+			locationMenu.Items.Add(changeItem);
+
+			var refreshItem = new MenuItem
+			{
+				Text = "Refresh",
+				Command = _vm.ReloadCommand
+			};
+			MenuItem.SetAccelerator(refreshItem, Accelerator.FromString("cmd+r"));
+			locationMenu.Items.Add(refreshItem);
+
+			mainMenu.Add(fileMenu);
+			mainMenu.Add(locationMenu);
+			SetMenu(Application.Current, mainMenu); // uncomment when we figure out why it blanks out the app
 		}
 	}
 }
